9c9a72d1abcb5ad57b8f679a8bf01602ef6cdf00
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-12-09 22:56:53 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const cursorSmall = document.querySelector(".small");
+const cursorBig = document.querySelector(".big");
+
+const positionElement = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", positionElement);
